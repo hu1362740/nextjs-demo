1f2d9d9d04af091ef689b916e429047f5b87614b
DESKTOP-DDJLKB3\China <hzq2006126@126.com>
2023-02-24 09:44:54 +0800

    feat：模板页面渲染

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// import '@/styles/globals.css'
+// import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+// export default function App({ Component, pageProps }: AppProps) {
+//   return <Component {...pageProps} />
+// }
+// .pages/_app.tsx
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Layout, ILayoutProps } from "@/pages/components/layout";
+
+const MyApp = (data: AppProps & ILayoutProps) => {
+  const { Component, pageProps, navbarData, footerData } = data;
+  console.log('data',data)
+  return (
+    <Layout navbarData={navbarData} footerData={footerData}>
+      <Component {...pageProps} />
+    </Layout>
+  );
+};
+export default MyApp;
